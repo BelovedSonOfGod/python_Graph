616334c7b31f0374a9260923cac8f8d57cb3aba7
Sebastian Silva <ssh.master@hotmail.com>
2025-11-05 22:23:29 -0600

    Added connection detection for two values

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from collections import deque
 
 
 graph = {
-    "A": ["B", "C"],
-    "B": ["A", "D"],
-    "C": ["A", "D"],
-    "D": ["B", "C"]
+    "A": ["B"],
+    "B": ["A", "C"],
+    "C": ["B"],
+    "D": ["E"],
+    "E": ["D", "F"],
+    "F": ["E"]
 }
 
 
@@ -36,6 +38,20 @@ def breadthFirst():
         setOfAlreadyVisited.add(currentValue) #Check if the value has been already added
 
 
+def connectionDetection(node1Value,node2Value)->True:
+    setOfAlreadyVisited = set()
+    stack=deque()
+    stack.append(node1Value) # Add the initial graph item
+    while stack:
+        currentValue=stack.pop()
+        if currentValue in setOfAlreadyVisited:
+            continue
+        if currentValue==node2Value:
+            return True
+        stack.extend(graph[currentValue]) #Include all the neighbors
+        setOfAlreadyVisited.add(currentValue) #Check if the value has been already added
+    return False
+
 
 
 
@@ -45,3 +61,8 @@ for node, neighbors in graph.items():
 print(depthFirst())
 print("-----------------------------")
 print(breadthFirst())
+print("-----------------------------")
+print("Is A connected to C? " + str(connectionDetection("A","C")))
+print("Is A connected to F? " + str(connectionDetection("A","F")))
+print("Is D connected to F? " + str(connectionDetection("D","F")))
+print("Is C connected to D? " + str(connectionDetection("C","D")))
