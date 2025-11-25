cdfb3614f6f73f11560c1e3ff34d635ec2a52318
Sebastian Silva <ssh.master@hotmail.com>
2025-11-24 20:19:20 -0600

    Added Djikstra functioning algorithm

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from collections import deque
-
+from typing import Dict, List, Tuple
+import heapq
 
 graph = {
     "A": ["B"],
@@ -83,14 +84,67 @@ def foundComponents()->list:
 
     return list_OfComponents
 
+
+
+def DjikstraAlgorithm(graphWithWeighs:Dict[str, List[Tuple[str, int]]], startingNode:str):
+    dist = {} # distancia mínima conocida a cada nodo
+    prev = {} # nodo anterior en el camino óptimo
+    heap = []   # 	elegir siempre el nodo más barato para explorar
+
+    # Inicializar distancias
+    for node in graphWithWeighs:
+        dist[node] = float('inf')
+        prev[node] = None
+
+    dist[startingNode] = 0
+
+    # Usar el heapq correctamente
+    heapq.heappush(heap, (0, startingNode))  # (distancia, nodo)
+    setOfAlreadyVisited = set()
+    # Y el loop
+    while heap:
+        currentDist, currentNode = heapq.heappop(heap)
+        if currentNode in setOfAlreadyVisited:
+            continue
         
+        for neighbor in graphWithWeighs[currentNode]:
+            accumulatedDistance=neighbor[1]+currentDist
+            if dist[neighbor[0]]>accumulatedDistance:
+                dist[neighbor[0]]=accumulatedDistance  # (distancia, nodo)
+                prev[neighbor[0]]=currentNode
+                heapq.heappush(heap, (neighbor[1]+currentDist, neighbor[0]))  # (distancia, nodo)
+
+        setOfAlreadyVisited.add(currentNode)
+    print(dist)
+    print(prev)
 
 
+
+
+'''
+dist = {}
+dist[start] = 0
+dist[otros] = ∞
+
+prev = {}
+prev[start] = None
+pending = []
+
+Guarda los nodos pendientes de procesar, ordenados automáticamente por el menor peso.
+heapq
+'''
 
 
 for node, neighbors in graph.items():
     print(f"{node} -> {neighbors}")
 
+graphWithWeighs= {
+    "A": [("B",2),("C",1)],
+    "B": [("D",1),("E",5),("A",2)],
+    "C": [("A",1),("D",3)], 
+    "D": [("B",1),("C",3)],         
+    "E": [("B",5)],
+}
 print(depthFirst())
 print("-----------------------------")
 print(breadthFirst())
@@ -100,3 +154,5 @@ print("Is A connected to F? " + str(connectionDetection("A","F")))
 print("Is D connected to F? " + str(connectionDetection("D","F")))
 print("Is C connected to D? " + str(connectionDetection("C","D")))
 print(foundComponents())
+
+DjikstraAlgorithm(graphWithWeighs,"A")
